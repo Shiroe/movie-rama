ca31d450f71dc23b04e2a6212977cd63110ccf7e
Shiroe <aggelosteck@gmail.com>
2022-12-23 15:52:52 +0200

    extracted components for movie card and navigation

---
+++ src/Navigation.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <nav className="w-full bg-gray-900 py-2">
+      <h1 className="mx-auto text-center text-emerald-500 lg:text-3xl xl:text-4xl">
+        MovieRama
+      </h1>
+    </nav>
+  );
+}
+
+export default Navigation;
---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+
+import type { Movie } from '../../pages/api/now_playing';
+import { GENRE } from '../../pages/api/genres';
+
+type MovieCardProps = {
+  movie: Movie;
+  genres: GENRE[];
+}
+
+const MovieCard = ({ movie, genres }: MovieCardProps) => {
+  return (
+    <div
+      key={movie.id}
+      className="flex flex-col justify-between rounded border-2 border-emerald-400 bg-emerald-700 relative"
+    >
+      <Image
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        width={720}
+        height={300}
+        alt="movie image"
+      />
+      <div className="flex flex-wrap justify-start gap-1 p-1 absolute top-0 left-0 right-0">
+        {movie.genre_ids.map((g) => (
+          <span
+            key={g}
+            className="rounded border border-emerald-400 p-1 text-sm font-semibold text-gray-800 bg-emerald-300 bg-opacity-100"
+          >
+            {genres?.filter(gen => gen.id === g)[0]?.name}
+          </span>
+        ))}
+      </div>
+      <div className="bg-emerald-500 py-1 text-center text-base text-gray-900">
+        <span className=''>
+          {new Date(movie.release_date).toLocaleDateString()}
+        </span>
+        <br />
+        <span className='font-semibold'>
+          {movie.title}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
