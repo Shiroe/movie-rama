70544632400daf808db29b54b20d6231ab14c8c7
Shiroe <aggelosteck@gmail.com>
2022-12-23 15:51:47 +0200

    added react-query and provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Inter } from '@next/font/google';
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
 });
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -18,7 +21,9 @@ export default function App({ Component, pageProps }: AppProps) {
           }
         `}
       </style>
-      <Component {...pageProps} className={`${inter.variable} font-sans`} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} className={`${inter.variable} font-sans`} />
+      </QueryClientProvider>
     </>
   );
 }
